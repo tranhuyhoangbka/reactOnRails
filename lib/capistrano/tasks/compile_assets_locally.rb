bb4d70f46edeaf9d7655a88da3b114d604ee0485
tranhuyhoangbka <hoangth92.nd@gmail.com>
2018-08-13 07:50:20 +0700

    fix task locale compile

--- lib/capistrano/tasks/compile_assets_locally.rb
+++ lib/capistrano/tasks/compile_assets_locally.rb
@@ -6,7 +6,7 @@ namespace :deploy do
     end
     on roles(:app) do |role|
       run_locally do
-        execute"rsync -av ./public/assets/ #{role.user}@#{role.hostname}:#{release_path}/public/assets/;"
+        execute"rsync -av ./public/assets/ deploy@#{role.hostname}:#{release_path}/public/assets/;"
       end
       sudo "chmod -R 755 #{release_path}/public/assets/"
     end
